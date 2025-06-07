eca822fdbf86ec5e8de9c86b5cc7b59744a5fe1c
ysskrishna <sivasaikrishnassk@gmail.com>
2025-06-07 16:12:31 +0530

    Siva | add parse_clerk_jwt logic

--- core/config.py
+++ core/config.py
@@ -5,3 +5,5 @@ load_dotenv()
 
 class Config:
     DATABASE_URL = os.getenv("DATABASE_URL")
+    CLERK_JWKS_URL=os.getenv("CLERK_JWKS_URL")
+    CLERK_ISSUER=os.getenv("CLERK_ISSUER")
--- core/jwtutils.py
+++ core/jwtutils.py
@@ -1,15 +1,35 @@
-from fastapi import Depends, HTTPException
+from fastapi import Depends, HTTPException, status
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from sqlalchemy.orm import Session
 from core.dbutils import get_db
 from models.models import User
+from jwt import PyJWKClient
+import jwt
+from core.config import Config
 
 
 security = HTTPBearer()
 
 
 def parse_clerk_jwt(credentials: HTTPAuthorizationCredentials = Depends(security)) -> dict:
-    pass
+    try:
+        token = credentials.credentials
+        audience = None
+        jwks_client = PyJWKClient(Config.CLERK_JWKS_URL)
+        signing_key = jwks_client.get_signing_key_from_jwt(token)
+        payload = jwt.decode(
+            token,
+            signing_key.key,
+            algorithms=["RS256"],
+            audience=audience,
+            issuer=Config.CLERK_ISSUER,
+        )
+        return payload
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=f"Invalid authentication credentials: {str(e)}",
+        )
 
 
 def get_current_user(
