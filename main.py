fab826b8172160a55734d15b72dd8faf4153666c
ysskrishna <sivasaikrishnassk@gmail.com>
2025-06-07 12:23:15 +0530

    Siva | create basic fastapi server

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-def main():
-    print("Hello from clerk-test!")
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+async def read_root():
+    return {"message": "Hello, FastAPI with uv!"}
 
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run(app, port=8001)
